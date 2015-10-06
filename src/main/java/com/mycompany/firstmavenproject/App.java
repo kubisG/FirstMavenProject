47d3628ee525e7fffd3ba8c90b2edd34fdd78868
KubisG <gois.ja89@gmail.com>
2015-10-06 16:29:28 +0200

    First commit

---
+++ src/main/java/com/mycompany/firstmavenproject/App.java
@@ -0,0 +1,33 @@
+package com.mycompany.firstmavenproject;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws AWTException
+    {
+        System.out.println( "Hello World!" );
+        
+        Robot robot = new Robot();
+        
+        WebDriver driver = new FirefoxDriver();
+        
+        driver.get("http://www.google.com");
+        driver.manage().window().maximize();
+        Point p = driver.findElement(By.id("lst-ib")).getLocation();
+        
+        robot.mouseMove(p.getX(), p.getY());
+        robot.delay(10000);
+        
+        driver.close();
+    }
+}
